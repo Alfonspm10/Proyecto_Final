db5ad8de81d207878cc606699dd07ebef9123e3a
Alfonso <alfonspm10@gmail.com>
2022-06-08 20:07:36 +0200

    socket connection (arreglado)
    Problemas con el broadcast de socket.io, solucionado cambiando path

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+const express = require('express');
+const http = require('http');
 const path = require('path');
-const express =  require('express');
+
+const { disconnect } = require('process');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-const PORT = 5000 || process.env.PORT;
+io.on('connection', socket => {
+    
+    socket.emit('message', 'Bienvenido a Wassup!');
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    socket.broadcast.emit('message', 'Un usuario se ha unido a la sala!');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'Un usuario ha abandonado el chat');
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
