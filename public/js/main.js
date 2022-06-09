5e65bdfa4ca30d62e11910f918585f9bce049970
Alfons <alfonspm@gmail.com>
2022-06-09 10:16:22 +0200

    login y register html integrado
    hay que corregir algunos 404 que popean en el js del login

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,15 @@ const socket = io();
 socket.on('message', message => {
     console.log(message);
 });
+
+
+window.onload = function () {
+
+    var erasebutton = document.getElementById("erase-task");
+
+    erasebutton.onclick = function () {
+        localStorage.clear()
+        location.reload();
+    }
+
+}
