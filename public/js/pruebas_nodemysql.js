03c26ddc8e1c0681a458331077f682add21e7cd4
Diegome <diego02260@gmail.com>
2022-06-14 10:26:42 +0200

    Intentos de conntect/register (mal)

--- public/js/pruebas_nodemysql.js
+++ public/js/pruebas_nodemysql.js
@@ -28,3 +28,55 @@ function imprimeUsuarios(result)
     }
 }
 
+
+
+
+
+con.connect(function(err) {
+  if (err) throw err;
+
+
+    var newUser = {
+        username: 'usuarioSecurisado',
+        pass: '1234',
+        salt: crypto.randomBytes(22).toString("hex"),
+    }
+
+    var queryIns = `
+    INSERT INTO usuarios
+    (username, hash, salt)
+    VALUES
+    (
+        '${newUser.username}',
+        '${crypto.createHash('sha256').update(newUser.pass + newUser.salt).digest('hex')}',
+        '${newUser.salt}'
+    );`;
+
+
+  con.query(queryIns, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+  var querySel = "SELECT * FROM balumba.usuarios;";
+
+  con.query(querySel, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    imprimeUsuarios(result)
+  });
+
+
+});
+
+
+
+
+
+
+/*
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+*/
